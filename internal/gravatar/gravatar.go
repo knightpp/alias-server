bf8d6e9efa793e77dae3da3169e2808880d7d5fa
Oleksii Proskurov <alexproskurov97@gmail.com>
2022-06-26 00:35:54 +0300

    implement gravatar url (#6)
    * implement gravatar url
    
    * fix gravatar func
    
    Co-authored-by: Oleksii Proskurov <oleksii.proskurov@connectwise.com>

--- internal/gravatar/gravatar.go
+++ internal/gravatar/gravatar.go
@@ -1,1 +1,17 @@
 package gravatar
+
+import (
+	"crypto/md5"
+	"strings"
+)
+
+func GetUrlOrDefault(email *string) string {
+	if email == nil {
+		return "https://www.gravatar.com/avatar/?d=wavatar"
+	}
+
+	r := md5.Sum([]byte(strings.TrimSpace(strings.ToLower(*email))))
+	imageURL := "https://www.gravatar.com/avatar/" + string(r[:]) + "?d=wavatar"
+
+	return imageURL
+}
